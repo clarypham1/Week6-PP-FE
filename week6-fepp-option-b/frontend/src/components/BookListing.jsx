65c24f3e89e3355acd29527a8e14ebc3359a1bb8
Saga <sagalehtonen@gmail.com>
2026-09-22 16:53:11 +0300

    feat(book-page): add route and fetch single book by id

--- week6-fepp-option-b/frontend/src/components/BookListing.jsx
+++ week6-fepp-option-b/frontend/src/components/BookListing.jsx
@@ -1,15 +1,17 @@
-
+import { Link } from "react-router-dom";
 
 const BookListings = ({ book }) => {
   return (
     <div className="book-preview">
-      <h2>{book.title}</h2>
+      <Link to={`/books/${book.id}`}>
+        <h2>{book.title}</h2>
+      </Link>
       <p>Author: {book.author}</p>
       <p>ISBN: {book.isbn}</p>
       <p>Publisher: {book.publisher}</p>
       <p>Genre: {book.genre}</p>
       <p>Available: {book.availability.isAvailable ? "Yes" : "No"}</p>
-    </div>
+    </div >
   );
 };
 
--- week6-fepp-option-b/frontend/src/pages/BookPage.jsx
+++ week6-fepp-option-b/frontend/src/pages/BookPage.jsx
@@ -1,19 +1,79 @@
+import { useParams, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+
 const BookPage = () => {
+  const navigate = useNavigate(); // now here!
+  const { id } = useParams();
+  const [book, setBook] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchBook = async () => {
+      try {
+        const res = await fetch(`/api/books/${id}`);
+        if (!res.ok) throw new Error("Network response was not ok");
+        const data = await res.json();
+        setBook(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchBook();
+  }, [id]);
+
+  const handleGoHome = () => {
+    navigate("/");
+  };
+
   return (
     <div className="book-preview">
-      <>
-        <h2></h2>
-        <p>Author: </p>
-        <p>ISBN: </p>
-        <p>Publisher: </p>
-        <p>Genre: </p>
-        <p>Available: </p>
-        <p>Due Date: </p>
-        <p>Borrower: </p>
-      </>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : (
+        <>
+          <h2>{book.title}</h2>
+          <p>Author: {book.author}</p>
+          <p>ISBN: {book.isbn}</p>
+          <p>Available: {book.availability.isAvailable ? "Yes" : "No"}</p>
+          <p>Due Date: {book.availability.dueDate
+            ? new Date(book.availability.dueDate).toLocaleDateString() : "-"}</p>
+          <p>Borrower: {book.availability.borrower || "-"}</p>
+          <button onClick={() => handleGoHome()}>Back</button>
+        </>
+      )}
     </div>
   );
 };
+
+/*
+  return
+  {
+    book && (
+      <div>
+        <>
+          <h2>{book.title}</h2>
+          <p>Author: {book.author}</p>
+          <p>ISBN: {book.isbn}</p>
+          <p>Publisher: {book.publisher}</p>
+          <p>Genre: {book.genre}</p>
+          <p>Available: {book.availability.isAvailable ? "Yes" : "No"}</p>
+          <p>Due Date: {book.availability.dueDate
+            ? new Date(book.availability.dueDate).toLocaleDateString() : "-"}</p>
+          <p>Borrower: {book.availability.borrower || "-"}</p>
+
+          <button onClick={() => Navigate("/")}>Back</button>
+        </>
+      </div>
+    )
+  };
+
+};*/
 
 export default BookPage;
 
