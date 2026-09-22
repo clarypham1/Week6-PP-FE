cb4d3ec5c885da4b4809031df40003dfd1df0720
Clary <claryphp@metropolia.fi>
2026-09-22 16:26:48 +0300

    feat(list-books): fetch and display all books on HomePage

--- week6-fepp-option-b/frontend/src/pages/HomePage.jsx
+++ week6-fepp-option-b/frontend/src/pages/HomePage.jsx
@@ -1,12 +1,37 @@
 import BookListings from "../components/BookListings";
+import { useState, useEffect } from 'react';
 
 const Home = () => {
+  const [books, setBooks] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const res = await fetch("/api/books");
+        if (!res.ok) throw new Error("Could not fetch books");
+        const data = await res.json();
+        setBooks(data);
+        setIsPending(false);
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        setIsPending(false);
+      }
+    };
+    fetchBooks();
+  }, []);
+
   return (
     <div className="home">
-      <BookListings />
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {books && <BookListings books={books} />}
     </div>
   );
 };
+
 
 export default Home;
 
